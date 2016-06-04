43fda8d47874c7a25f714971f0d1cb526ff3cd5b
Wil Santana <santana.wil@gmail.com>
2016-06-04 17:30:12 +0000

    Bootstrap installed

---
+++ app/controllers/gardens_controller.rb
@@ -0,0 +1,6 @@
+class GardensController < ApplicationController
+
+	def index
+	end
+	
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Waynesgarden::Application.routes.draw do
+  root 'gardens#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
