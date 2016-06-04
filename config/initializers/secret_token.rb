da72159c2616b23ea28494b09714a272b41c17c9
Wil Santana <santana.wil@gmail.com>
2016-06-04 16:47:54 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Waynesgarden::Application.config.secret_key_base = '8c4a727c319e1fef640d1b7894b43458e591beaae433b0fafce118a96fbfa85b2bfd7d675c38869e4b042158fafcf18cb43c1544b6fe67d92c428b4c50a55422'
